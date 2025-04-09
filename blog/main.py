b0379282b200fd49da8196528018d226700ecdec
Gautam Joshi <gautam.joshii1905@gmail.com>
2025-04-09 23:28:45 +0530

    Update main.py

--- blog/main.py
+++ blog/main.py
@@ -8,5 +8,5 @@ model.database.Base.metadata.create_all(bind=engine)       #importing All databa
 
 app=FastAPI()
 
-app.include_router(blog.router)                         #routes the control to the specific module of router directory
+app.include_router(blog.router)                         #routes the control to the specific module of routers directory
 app.include_router(user.router)
